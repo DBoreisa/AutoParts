a46c67f2bc330034bf7b699b3d6a2eeef87efee1
Dovydas <dovydas.boreisa@gmail.com>
2025-06-19 19:42:48 +0300

    cart saugomas local storage

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,10 +1,20 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
 export const CartProviderWrapper = ({ children }) => {
-    const [cart, setCart] = useState([]);
-    const [showCart, setShowCart] = useState(false);
+  const [cart, setCart] = useState(() => {
+    const storedCart = localStorage.getItem("cart");
+    //console.log("Init cart from localStorage:", storedCart);
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
+
+  const [showCart, setShowCart] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+    //console.log("Saving to localStorage:", cart);
+  }, [cart]);
 
     return (
         <CartContext.Provider value={{ cart, setCart, showCart, setShowCart }}>
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -3,14 +3,18 @@ import { useCartContext } from "../contexts/CartContext";
 const useCart = (item) => {
     const { cart, setCart } = useCartContext();
 
+    const isInCart = item && cart.findIndex(c => c.id === item.id) >= 0; //patikrina ir item jau idetas
+
     const addToCart = () => {
-        cart.findIndex( c => c.id === item.id) >= 0 
-            ? setCart(cart.filter(c => c.id !== item.id)) // Jei item jau yra, tai istrina
-            : setCart(c => [...c, item]); // Prideda, jei dar nepridetas
+      if (!item) return; // jei item == undefined nevygdo funkcijos
+        if (isInCart) {
+            setCart(cart.filter(c => c.id !== item.id)); // pasalina jei jau buvo idetas
+        } else {
+            setCart(c => [...c, item]); // prideda jei nebuvo
+        }
     };
 
-    const addToCartText = cart.findIndex( c => c.id === item.id) >= 0 
-        ? "Remove from cart" : "Add to cart";
+    const addToCartText = isInCart ? "Remove from cart" : "Add to cart";
 
     return { addToCart, addToCartText }
 };
