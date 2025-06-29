4fa94917d2f7ea7ed84431d2161cf6efbb3c28c4
Dovydas <dovydas.boreisa@gmail.com>
2025-06-29 19:46:18 +0300

    valiutos pasirinkimo switchas

--- src/components/CurrencySwitch/CurrencySwitch.jsx
+++ src/components/CurrencySwitch/CurrencySwitch.jsx
@@ -3,7 +3,6 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import useCurrencyRate from "../../hooks/useCurrencyRate";
 import { useCurrencyContext } from "../../contexts/CurrencyContext";
 
 const CurrencySwitch = () => {
@@ -13,18 +12,44 @@ const CurrencySwitch = () => {
         setCurrency(event.target.value);
     };
     return (
-        <FormControl sx={{ m: 1, minWidth: 60 }} size="small">
-            <InputLabel id="demo-select-small-label">Currency</InputLabel>
+        <FormControl variant="standard"
+            size="small"
+            sx={{
+                minWidth: 60,
+                color: "white",
+                "& .MuiInputBase-root": {
+                border: "none",
+                "&:before": { borderBottom: "none" },
+                "&:after": { borderBottom: "none" },
+                "&:hover:not(.Mui-disabled):before": { borderBottom: "none" },
+                },
+            }}
+        >
+            <InputLabel 
+                id="demo-select-small-label"
+                sx={{ color: "white" }}
+            >
+                Currency:
+            </InputLabel>
             <Select
                 labelId="demo-select-small-label"
                 id="demo-select-small"
                 value={currency}
                 label="Currency"
                 onChange={handleChange}
+                disableUnderline
+                sx={{
+                    color: "white",                      // text color
+                    "& .MuiSvgIcon-root": {
+                    color: "white",                   // arrow color
+                    },
+                    "&::before": { borderBottom: "none" },
+                    "&::after": { borderBottom: "none" },
+                }}
             >
-                <MenuItem value="EUR">EUR (€)</MenuItem>
-                <MenuItem value="USD">USD ($)</MenuItem>
-                <MenuItem value="GBP">GBP (£)</MenuItem>
+                <MenuItem value="EUR">EUR</MenuItem>
+                <MenuItem value="USD">USD</MenuItem>
+                <MenuItem value="GBP">GBP</MenuItem>
             </Select>
         </FormControl>
     );
