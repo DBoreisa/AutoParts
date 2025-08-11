fb614d3d069ba835ad4379fbdf96a012dcc101ae
Dovydas <dowe892@gmail.com>
2025-08-11 11:28:29 +0300

    sutavarkyta RecentlyAdded

--- src/components/RecentlyAdded/RecentlyAdded.jsx
+++ src/components/RecentlyAdded/RecentlyAdded.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box, Grid2 } from "@mui/material";
-import cars from "../../data/carsData";
+import useProducts from "../../hooks/useProducts"
 import ItemCard from "../ItemCard";
 import { useNavigate } from "react-router-dom";
 
 const RecentlyAdded = () => {
     const navigate = useNavigate();
+    const products = useProducts();
 
     const handleClick = (id) => {
         navigate(`/details/${id}`);
@@ -26,16 +27,16 @@ const RecentlyAdded = () => {
                 justifyContent="center"
                 alignItems="center"
             >
-            {cars.slice(0, 4).map((car) => (
+            {products.slice(0, 4).map((product) => (
                 <Grid2 
                     item 
                     xs={12} 
                     sm={6} 
                     md={6} 
-                    key={car.id}
-                    onClick={() => handleClick(car.id)}
+                    key={product.id}
+                    onClick={() => handleClick(product.id)}
                 >
-                    <ItemCard name={car.name} price={car.price} img={car.image[0]}/>
+                    <ItemCard name={product.name} price={product.price} img={product.images[0]?.image}/>
                 </Grid2>
             ))}
             </Grid2>
