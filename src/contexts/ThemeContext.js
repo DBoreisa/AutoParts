73f900c96992d0ec0beaafbdc0b29e28e5ceff48
Dovydas <dowe892@gmail.com>
2025-10-06 15:24:25 +0300

    them saugosi local storage

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,15 +1,22 @@
-import React, { createContext, useMemo, useState } from "react";
+import React, { createContext, useMemo, useState, useEffect } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import getTheme from "../theme/getTheme";
 
 export const ThemeContext = createContext();
 
 export const ThemeProviderWrapper = ({ children }) => {
-  const [mode, setMode] = useState("dark");
+  const [mode, setMode] = useState(() => {
+    const savedMode = localStorage.getItem("themeMode");
+    return savedMode ? JSON.parse(savedMode) : "dark";
+  });
 
   const toggleTheme = () => {
     setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
   };
+
+  useEffect(() => {
+    localStorage.setItem("themeMode", JSON.stringify(mode));
+  }, [mode]);
 
   const theme = useMemo(() => getTheme(mode), [mode]);
 
